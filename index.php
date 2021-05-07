ecbdbb86f3de8305f0a9f3785032197cec268cea
Michael Janes-Villiard <michael.janesv@gmail.com>
2021-05-07 11:18:28 -0400

    Ajout page index.php

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TimConf</title>
+
+    <link rel="stylesheet" href="styles/main.css" />
+
+    <script src="scripts/main.js" defer></script>
+</head>
+
+<body>
+    <div class="site-container main-grid">
+        <header class="header wrapper">
+            <a href="./index.html"><img src="assets/images/svg_logo_timconf.svg" alt="logo TimConf" />
+            </a>
+            <nav class="menu">
+                <div class="label"></div>
+                <label for="toggle">&#9776;</label>
+                <input type="checkbox" id="toggle" />
+                <div class="bouton_nav">
+                    <a href="./index.html" class="menu_item">Accueil</a>
+                    <a href="./conferencier.html" class="menu_item">À propos</a>
+                    <a href="./conferencier.html" class="menu_item">Conférenciers</a>
+                    <a href="./conferencier.html" class="menu_item">Conférences</a>
+                    <a href="./index.html#forfaits" class="menu_item">Forfaits</a>
+                </div>
+            </nav>
+        </header>
+
+        <div class="hero wrapper">
+            <div class="content-hero">
+                <h1>3D vs. Réalité</h1>
+                <div class="date">01 - 02 - 03 Août 2021</div>
+                <div class="descrip">
+                    La conférence #1 pour les amateurs et les pros de
+                    l’industrie. Venez apprendre des nouveaux trucs et vous
+                    mettre à jour sur les dernières technologies utilisé
+                    dans le monde du 3D.
+                </div>
+                <div class="pos-bouton">
+                    <a href="" class="bouton">
+                        <p>Forfaits</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <section class="partenaires">
+            <div class="wrapper">
+                <h2>Nos partenaires</h2>
+                <div class="part-sect1">
+                    <img src="assets/images/img_logo_adobe.png" alt="logo adobe" class="logo_partenaires1" />
+                    <img src="assets/images/img_logo_substance.png" alt="logo substance" class="logo_partenaires2" />
+                    <img src="assets/images/img_logo_autodesk.png" alt="logo autodesk" class="logo_partenaires3" />
+                </div>
+
+                <div class="part-sect2">
+                    <img src="assets/images/img_logo_blender.png" alt="logo
+                        blender" class="logo_partenaires4" />
+                    <img src="assets/images/img_logo_zbrush.png" alt="logo zbrush" class="logo_partenaires5" />
+                </div>
+            </div>
+        </section>
+
+        <section class="forfaits" id="forfaits">
+            <div class="wrapper">
+                <h2 class="titre-forfait">Nos forfaits</h2>
+                <div class="nos_forfaits">
+                    <div class="forfait package1">
+                        <h2>Billets<br />une journée</h2>
+                        <ul>
+                            <li>accès au site une journée</li>
+                            <li>accès aux comférences</li>
+                            <li>rencontre avec les conférenciers</li>
+                        </ul>
+                        <a href="">
+                            <div class="pos-bouton-forfait">
+                                <div class="bouton">Acheter</div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="forfait package2">
+                        <h2>
+                            Billets <br />
+                            2 jours
+                        </h2>
+                        <ul>
+                            <li>accès au site deux journées</li>
+                            <li>accès aux comférences</li>
+                            <li>rencontre avec les conférenciers</li>
+                        </ul>
+                        <a href="">
+                            <div class="pos-bouton-forfait">
+                                <div class="bouton">Acheter</div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="forfait package3">
+                        <h2>
+                            Billets <br />
+                            3 jours
+                        </h2>
+                        <ul>
+                            <li>accès au site trois journées</li>
+                            <li>accès aux comférences</li>
+                            <li>rencontre avec les conférenciers</li>
+                        </ul>
+                        <a href="">
+                            <div class="pos-bouton-forfait">
+                                <div class="bouton">Acheter</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="conferenciers">
+            <div class="wrapper">
+                <h2>Nos Conférenciers</h2>
+
+                <div class="grid_conf">
+                    <a href="">
+                        <div class="speaker">
+                            <img src="assets/images/img_michael.png" alt="image conférencier" />
+                            <h4>Michaël Janes-Villiard</h4>
+                            <div class="title-speaker">Modélisateur 3D</div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="speaker">
+                            <img src="assets/images/img_noemie.png" alt="image conférencier" />
+                            <h4>Noémie Tremblay</h4>
+                            <div class="title-speaker">Artiste</div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="speaker">
+                            <img src="assets/images/img_julien.png" alt="image conférencier" />
+                            <h4>Julien Lacroix</h4>
+                            <div class="title-speaker">Artiste</div>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="speaker">
+                            <img src="assets/images/img_eleanor.png" alt="image conférencier" />
+                            <h4>Eleanor Rigby</h4>
+                            <div class="title-speaker">
+                                Modélisatrice 3D
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="speaker">
+                            <img src="assets/images/img_lucas.png" alt="" />
+                            <h4>Lucas Gagnon</h4>
+                            <div class="title-speaker">Artiste</div>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="speaker">
+                            <img src="assets/images/img_william.png" alt="" />
+                            <h4>William Peak</h4>
+                            <div class="title-speaker">Modélisateur 3D</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div class="wrapper">
+                <a href=""><img src="assets/images/svg_logo_timconf.svg" alt="logo TimConf" /></a>
+                <div class="menu_footer">
+                    <ul>
+                        <li>
+                            <a href="./index.html">Accueil</a>
+                        </li>
+                        <li>
+                            <a href="./conferencier.html"> À propos</a>
+                        </li>
+                        <li>
+                            <a href="./conferencier.html">Conférenciers</a>
+                        </li>
+                        <li>
+                            <a href="./conferencier.html">Conférences</a>
+                        </li>
+                        <li>
+                            <a href="./index.html#forfaits">Forfaits</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="ligne"></div>
+                <div class="pos-footer">
+                    <div class="icons">
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <div class="size instagram"></div>
+                        </a>
+                        <a href="https://dribbble.com/" target="_blank">
+                            <div class="size dribble"></div>
+                        </a>
+                        <a href="https://twitter.com/HPHIGHPEN" target="_blank">
+                            <div class="size twitter"></div>
+                        </a>
+                        <a href="https://www.youtube.com/" target="_blank">
+                            <div class="size youtube"></div>
+                        </a>
+                    </div>
+                    <div class="copyrights">
+                        © 2020 TimConf. Tous droits réservés
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
