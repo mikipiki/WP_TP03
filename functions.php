0542602753a1d08978b1deb8306b91120fd1f039
Michael Janes-Villiard <michael.janesv@gmail.com>
2021-05-15 22:34:58 -0400

    Ajout functions + ajout cpt conférenciers et conférences dans function.php

---
+++ functions.php
@@ -0,0 +1,113 @@
+<?php
+
+// Register Custom Post Type
+function wp_conferencier() {
+
+	$labels = array(
+		'name'                  => _x( 'Conferenciers', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Conferencier', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Conferenciers', 'text_domain' ),
+		'name_admin_bar'        => __( 'Post Type', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'Tous les conferenciers', 'text_domain' ),
+		'add_new_item'          => __( 'Ajouter un nouveau conferencier', 'text_domain' ),
+		'add_new'               => __( 'Ajouter conferencier', 'text_domain' ),
+		'new_item'              => __( 'nouveau conferencier', 'text_domain' ),
+		'edit_item'             => __( 'Editer conferencier', 'text_domain' ),
+		'update_item'           => __( 'Update conferencier', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Conferencier', 'text_domain' ),
+		'description'           => __( 'les conferenciers', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'wp_conferencier', $args );
+
+}
+add_action( 'init', 'wp_conferencier', 0 );
+
+// Register Custom Post Type
+function wp_conferences() {
+
+	$labels = array(
+		'name'                  => _x( 'Conferences', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Conference', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Conférences', 'text_domain' ),
+		'name_admin_bar'        => __( 'Post Type', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'Toutes les conférences', 'text_domain' ),
+		'add_new_item'          => __( 'Ajouter une nouvelle conférence', 'text_domain' ),
+		'add_new'               => __( 'Ajouter conférence', 'text_domain' ),
+		'new_item'              => __( 'nouvelle conférence', 'text_domain' ),
+		'edit_item'             => __( 'Editer une conférence', 'text_domain' ),
+		'update_item'           => __( 'Update conference', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Conference', 'text_domain' ),
+		'description'           => __( 'les conferences', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'wp_conferences', $args );
+
+}
+add_action( 'init', 'wp_conferences', 0 );
